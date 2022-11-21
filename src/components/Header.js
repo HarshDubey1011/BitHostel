8172b4566a79a95db4076822f2c26287e56470e5
AnthoniKindo <anthonikindo05@gmail.com>
2022-11-21 23:39:07 +0530

    done some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,14 @@ const Header = () => {
 	return (
 			<div className="Header">
 				<div className = "box">
-					<div className="fixedWidth">
-					<p className="text">Welcome To <small>BIT Raipur Boys Hostel</small></p>
-					<hr width="55%" />
-					<p className="text text2">Be A Catalyst And Create Change In The Lives Of Academically Brilliant Students With High Potential</p>
-					<div className="links">
-					<a href="#">Submit</a>
-					<a href="#">Submit</a>
-					<a href="#">Apply Now</a>
-					<a href="#">Login</a>
-					</div>
+					<p className="text">Welcome To <small><br />BIT Raipur Boys Hostel</small></p>
+					{/* <p className="text text2">Be A Catalyst And Create Change In The Lives Of Academically Brilliant Students With High Potential</p> */}
 				</div>
+				<div className="links">
+					<button>Submit</button>
+					<button>Submit</button>
+					<button>Apply Now</button>
+					<button>Login</button>
 				</div>
 			</div>
 			)
